8cab2f6a8c4ac3a74a1c807787c208e0e215d1a9
Tom Boettger <t.boettger@live.de>
2021-02-11 17:30:00 +0100

    Add context loading test for the controller

--- src/test/java/de/otto/codingchallenge/ipranges/IprangesApplicationTests.java
+++ src/test/java/de/otto/codingchallenge/ipranges/IprangesApplicationTests.java
@@ -1,13 +1,21 @@
 package de.otto.codingchallenge.ipranges;
 
+import de.otto.codingchallenge.ipranges.controller.IpRangesController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class IprangesApplicationTests {
 
+	@Autowired
+	private IpRangesController controller;
+
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
