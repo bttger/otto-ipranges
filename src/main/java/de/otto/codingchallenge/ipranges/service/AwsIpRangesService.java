e98e5db5f95a03ebb8ba2368fbdc319f5fcaf23a
Tom Boettger <t.boettger@live.de>
2021-02-11 17:06:42 +0100

    Implement the ip-ranges service methods

--- src/main/java/de/otto/codingchallenge/ipranges/service/AwsIpRangesService.java
+++ src/main/java/de/otto/codingchallenge/ipranges/service/AwsIpRangesService.java
@@ -9,10 +9,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.Duration;
+import java.util.Locale;
+import java.util.Optional;
 
 @Service
 public class AwsIpRangesService {
 
+    /**
+     * Possible regions for the AWS IP-ranges API.
+     */
     public enum Region {
         EU, US, AP, CN, SA, AF, CA, ALL
     }
@@ -36,15 +41,50 @@ public class AwsIpRangesService {
                 .build();
     }
 
+    /**
+     * Fetches the IP-ranges (in CIDR notation) for a corresponding region from AWS.
+     *
+     * @param region For which region it should fetch the IP-ranges.
+     * @return A String containing all IP-ranges of a particular {@link Region}. Each entry ends with a new line.
+     */
     public String getIpRangesForRegion(Region region) {
-        IpRangesDto resp = this.restTemplate.getForObject(this.apiUrl, IpRangesDto.class);
-        if (resp == null) {
+        IpRangesDto ipRanges = this.restTemplate.getForObject(this.apiUrl, IpRangesDto.class);
+        if (ipRanges == null) {
             return "";
         }
 
         StringBuilder builder = new StringBuilder();
-        resp.getIpRanges().forEach(range -> builder.append(range.getRange()).append(":").append(range.getRegion()).append("\n"));
+        ipRanges.getIpRanges().forEach(range -> {
+            Optional<Region> entryRegion = extractRegionFromString(range.getRegion());
+            if (entryRegion.isPresent() && (Region.ALL == region || entryRegion.get() == region)) {
+                builder.append(range.getRange()).append("\n");
+            }
+        });
 
         return builder.toString();
+    }
+
+    /**
+     * Extracts a {@link Region} from a string, which describes a Region within its first two letters. It can only
+     * extract a {@link Region} if the enum holds a corresponding entry.
+     *
+     * @param region A string with the first two letters describing the region. E.g. 'us-east-2'.
+     * @return An Optional object which holds a {@link Region} and that isPresent if a {@link Region} could be
+     * extracted from the region parameter. If not, the Optional return value isEmpty.
+     */
+    private Optional<Region> extractRegionFromString(String region) {
+        if (region.equals("GLOBAL")) {
+            return Optional.empty();
+        }
+        String regionPrepared = region.toUpperCase(Locale.ENGLISH).substring(0, 2);
+
+        Region regionReturn;
+        try {
+            regionReturn = Region.valueOf(regionPrepared);
+        } catch (IllegalArgumentException ex) {
+            return Optional.empty();
+        }
+
+        return Optional.of(regionReturn);
     }
 }
