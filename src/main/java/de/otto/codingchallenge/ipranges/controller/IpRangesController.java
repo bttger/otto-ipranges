ec4c73f9a38ce288ec1802cd83293bf5e8320bfe
Tom Boettger <t.boettger@live.de>
2021-02-11 15:30:58 +0100

    Implement exception handler for missing query string param

--- src/main/java/de/otto/codingchallenge/ipranges/controller/IpRangesController.java
+++ src/main/java/de/otto/codingchallenge/ipranges/controller/IpRangesController.java
@@ -1,5 +1,7 @@
 package de.otto.codingchallenge.ipranges.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -8,6 +10,13 @@ public class IpRangesController {
     @GetMapping(value = "/", produces = "text/plain;charset=utf-8")
     public String ipRanges(@RequestParam String region) {
         return region;
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleMissingParams(MissingServletRequestParameterException ex) {
+        String param = ex.getParameterName();
+        return String.format("missing query string parameter: '%s'", param);
     }
 
 }
