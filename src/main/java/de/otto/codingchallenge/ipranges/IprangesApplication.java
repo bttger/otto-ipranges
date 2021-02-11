eccdb3263db9510db9661544bbe41db3d8e4bcaa
Tom Boettger <t.boettger@live.de>
2021-02-11 14:58:04 +0100

    Set up the project with spring initializer

---
+++ src/main/java/de/otto/codingchallenge/ipranges/IprangesApplication.java
@@ -0,0 +1,13 @@
+package de.otto.codingchallenge.ipranges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IprangesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IprangesApplication.class, args);
+	}
+
+}
